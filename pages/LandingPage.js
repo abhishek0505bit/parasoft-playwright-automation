df02bbbd9cf317453086fc92e996384e427def86
Abhishek Yadav <abhishek.yadav@doubleverify.com>
2025-04-12 11:16:18 +0530

    page: added LandingPage.js

---
+++ pages/LandingPage.js
@@ -0,0 +1,42 @@
+
+
+/**
+ * username
+ * password
+ * login button
+ * register button
+ */
+
+import {Page, Locator} from '@playwright/test';
+
+export class LandingPage{
+    constructor(page)
+    {
+        this.page = page; 
+        this.usernameInput = page.locator('input[name = "username"]')
+        this.passwordInput = page.locator('input[name = "password"]')
+        this.loginButton = page.locator('input[value = "Log In"]')
+        this.registerButton = page.locator('a[href="register.htm"]')
+        this.url = 'https://parabank.parasoft.com/'
+
+    }
+
+    async navigate(){
+        await this.page.goto(this.url);
+
+    }
+
+    async login(username, password){
+        await this.usernameInput.fill(username);
+        await this.passwordInput.fill(password);
+        await this.loginButton.click(); 
+        
+
+    }
+
+    async goToRegisterPage()
+    {
+        await this.registerButton.click();
+
+    }
+}
