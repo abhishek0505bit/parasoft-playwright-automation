9d2c838be1ccb87382ef01b1cf1be13471bc6f96
Abhishek Yadav <abhishek.yadav@doubleverify.com>
2025-04-13 14:54:05 +0530

    refactor: page file, added log statement at every key step

--- pages/LandingPage.js
+++ pages/LandingPage.js
@@ -1,16 +1,8 @@
 
 
-/**
- * username
- * password
- * login button
- * register button
- */
-
-export class LandingPage{
-    constructor(page)
-    {
-        this.page = page; 
+export class LandingPage {
+    constructor(page) {
+        this.page = page;
         this.usernameInput = page.locator('input[name = "username"]')
         this.passwordInput = page.locator('input[name = "password"]')
         this.loginButton = page.locator('input[value = "Log In"]')
@@ -19,21 +11,29 @@ export class LandingPage{
 
     }
 
-    async navigate(){
+    async navigate() {
+
+        console.log(`Navigating to url: ${this.url}`)
         await this.page.goto(this.url);
 
     }
 
-    async login(username, password){
+    async fillLoginDetails(username, password) {
+
+        console.log(`Filling Login details with username: ${username} and password: ${password}`)
         await this.usernameInput.fill(username);
         await this.passwordInput.fill(password);
-        await this.loginButton.click(); 
-        
 
     }
 
-    async goToRegisterPage()
-    {
+    async clickLogin() {
+
+        console.log(`Clicking on Log in Button`)
+        await this.loginButton.click();
+    }
+
+    async goToRegisterPage() {
+        console.log(`Clicking on Register Button.`)
         await this.registerButton.click();
 
     }
